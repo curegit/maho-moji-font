4f043098bf7ff7e6d57777b3edca3218fb5e937c
curegit <37978051+curegit@users.noreply.github.com>
2024-10-15 22:28:35 +0900

    Use realpath in install scripts for clarity

--- install_system.sh
+++ install_system.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cd "$(dirname "$0")"
+cd "$(realpath "$(dirname "$0")")"
 sudo mkdir -p /usr/share/fonts/opentype/maho
 sudo cp Maho-Moji.otf /usr/share/fonts/opentype/maho/
 sudo fc-cache -rv /usr/share/fonts/opentype/maho/
--- install_user.sh
+++ install_user.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-cd "$(dirname "$0")"
+cd "$(realpath "$(dirname "$0")")"
 mkdir -p ~/.local/share/fonts
 cp Maho-Moji.otf ~/.local/share/fonts/
 fc-cache -rv ~/.local/share/fonts/
